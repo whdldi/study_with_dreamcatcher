54a59d47266315f5bf7d74471f9d8dc56f3ec283
Hesper_03 <85474393+whdldi@users.noreply.github.com>
2023-10-01 04:20:06 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -59,7 +59,7 @@
 
 
           // |￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣|
-          //     Pointer cursor (다시체크)
+          //     Pointer cursor 
           // |＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿|
           //    \ (•◡•) /
 
@@ -138,7 +138,16 @@
                // 드래그 가능한 요소 생성
                $(".draggable_clock").draggable({
                     touchAction: "auto" // 터치 이벤트 활성화
+                         ,
+                    grid: [10, 10] // 스냅
                });
+               $(".draggable_bgm").draggable({
+                    touchAction: "auto" // 터치 이벤트 활성화
+                         ,
+                    grid: [10, 10] // 스냅
+               });
+
+
 
                // 드롭 영역 설정
                $("#dropArea").droppable({
@@ -148,14 +157,9 @@
                     }
                });
 
-               // 숨김 버튼 클릭 시 요소 숨기기
-               /*  $("#hideBtn").click(function () {
-                     $(".draggable_clock").hide();
-                }); */
-               // 보이기 버튼 클릭 시 요소 보이기
-               /*  $("#showBtn").click(function () {
-                     $(".draggable_clock").show();
-                }); */
+
+
+
           });
 
 
@@ -298,6 +302,148 @@
 
 
 
+
+
+
+
+
+          // |￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣|
+          //     drag_box 2.BGM Playlist 
+          // |＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿|
+          //    \ (•◡•) /
+
+
+          $(document).ready(function () {
+               $(".drag_box_inner_btn .box_playlist").click(function () {
+                    let img = $(this).find("img");
+                    if (img.hasClass("widget_active_bgm")) {
+                         img.removeClass("widget_active_bgm");
+                    } else {
+                         img.addClass("widget_active_bgm");
+                    }
+               });
+          });
+          $(document).ready(function () {
+               $(".box_playlist").click(function () {
+                    if ($(this).children('img').hasClass("widget_active_bgm")) {
+                         $(".draggable_bgm").css("display", "block");
+                         /* 여기에서 block되면 재생 */
+
+                         // 오디오 재생
+                         /* const audio = document.getElementById('bgm-audio');
+                         audio.play(); */
+                    } else {
+                         $(".draggable_bgm").css("display", "none");
+                         /* 여기에서 none되면 정지 */
+
+                         // 오디오 정지
+                         /*   const audio = document.getElementById('bgm-audio');
+                           audio.pause(); */
+                    }
+               });
+          });
+
+
+
+
+
+
+
+          // |￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣|
+          //     BGM Playlist 
+          // |＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿|
+          //    \ (•◡•) /
+          $(document).ready(function () {
+               const audio = document.getElementById('bgm-audio');
+               const playPauseButton = document.getElementById('play-pause-button');
+               const currentTrack = document.getElementById('current-track');
+               const remainingTime = document.getElementById('remaining-time');
+               const progressBar = document.getElementById('progress-bar');
+               const volumeSlider = document.getElementById('volume-slider');
+               const tracks = ['Fireplace', 'Rain', 'Keyboard Typing']; // 확장자를 제외한 파일 이름만 저장
+               let currentTrackIndex = 0;
+
+               function updateTrackInfo() {
+                    // .n초 후에 곡명을 업데이트 (남은시간 NaN뜨는거 때문에 느리게 바꿈)
+                    setTimeout(() => {
+                         currentTrack.textContent = '' + tracks[currentTrackIndex];
+                    }, 180);
+               }
+
+               function playNextTrack() {
+                    currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
+                    updateTrackInfo(); // 곡명을 먼저 업데이트
+                    audio.src = 'bgm/' + tracks[currentTrackIndex] + '.mp3'; // 확장자 추가
+                    audio.load(); // 새로운 오디오 파일 로드
+                    audio.play();
+               }
+
+               function updateRemainingTime() {
+                    if (!isNaN(audio.duration)) { // duration이 NaN이 아닌 경우에만 실행
+                         const duration = audio.duration;
+                         const currentTime = audio.currentTime;
+                         const remaining = duration - currentTime;
+                         const minutes = Math.floor(remaining / 60);
+                         const seconds = Math.floor(remaining % 60);
+                         remainingTime.textContent = '' + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+
+                         // 업데이트 프로그래스 바
+                         progressBar.value = (currentTime / duration) * 100;
+
+                    }
+               }
+
+               function updateVolume() {
+                    audio.volume = volumeSlider.value / 100;
+               }
+
+               audio.addEventListener('ended', playNextTrack);
+               audio.addEventListener('timeupdate', updateRemainingTime);
+
+               // 오디오 파일이 로드되었을 때 트랙 정보 업데이트
+               audio.addEventListener('canplay', function () {
+                    updateTrackInfo();
+               });
+
+               $('#play-pause-button').click(function () {
+                    if (audio.paused) {
+                         audio.play();
+                         playPauseButton.innerHTML = '<i class="fas fa-pause"></i>';
+                    } else {
+                         audio.pause();
+                         playPauseButton.innerHTML = '<i class="fas fa-play"></i>';
+                    }
+               });
+
+               $('#prev-button').click(function () {
+                    currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+                    updateTrackInfo(); // 곡명을 먼저 업데이트
+                    audio.src = 'bgm/' + tracks[currentTrackIndex] + '.mp3'; // 확장자 추가
+                    audio.load(); // 새로운 오디오 파일 로드
+                    audio.play();
+               });
+
+               $('#next-button').click(playNextTrack);
+
+               // 프로그래스 바 클릭 이벤트 처리
+               progressBar.addEventListener('click', function (e) {
+                    const progressBarRect = progressBar.getBoundingClientRect();
+                    const clickX = e.clientX - progressBarRect.left;
+                    const progressBarWidth = progressBarRect.width;
+                    const seekTime = (clickX / progressBarWidth) * audio.duration;
+                    audio.currentTime = seekTime;
+               });
+
+               // 볼륨 슬라이더 이벤트 처리
+               // volumeSlider.addEventListener('input', updateVolume);
+          });
+
+
+
+
+
+
+
           // |￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣|
           //     drag_box 3.나
           // |＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿|
@@ -309,15 +455,14 @@
                $(".drag_box_inner_btn .box_me").click(function () {
                     let img = $(this).find("img");
                     if (img.hasClass("widget_active_me")) {
-                        img.removeClass("widget_active_me");
-                        $(".cam_me").hide();
-                        $(".cam_one_by_one").css("grid-template-columns", "none"); // CSS 값을 변경
+                         img.removeClass("widget_active_me");
+                         $(".cam_me").hide();
+                         $(".cam_one_by_one").css("grid-template-columns", "none"); // CSS 값을 변경
                     } else {
-                        img.addClass("widget_active_me");
-                        $(".cam_me").show();
-                        $(".cam_one_by_one").css("grid-template-columns", "repeat(2, 1fr)"); // CSS 값을 변경
+                         img.addClass("widget_active_me");
+                         $(".cam_me").show();
+                         $(".cam_one_by_one").css("grid-template-columns", "repeat(2, 1fr)"); // CSS 값을 변경
                     }
-                });
-                
-          });
+               });
 
+          });
